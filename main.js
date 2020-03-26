7b1a2e2a5f09a85964d1420f2c2ade205f277965
CoreNebula <62679387+CoreNebula@users.noreply.github.com>
2020-03-26 11:28:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function sortTable() {
   var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById("myTable");
+  table = document.getElementById("table1");
   switching = true;
   while (switching) {
     switching = false;
     rows = table.rows;
     for (i = 1; i < (rows.length - 1); i++) {
       shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
+      x = rows[i].getElementsByTagName("TH")[0];
+      y = rows[i + 1].getElementsByTagName("TH")[0];
       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
         shouldSwitch = true;
         break;
