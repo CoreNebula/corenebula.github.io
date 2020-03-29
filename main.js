ae85900454a1728b17820b5b520fd2145c258aec
CoreNebula <62679387+CoreNebula@users.noreply.github.com>
2020-03-29 04:25:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -56,12 +56,3 @@ function includeHTML() {
     }
   }
 }
-$(document).ready(function() {
-  $('.open-menu').on('click', function() {
-     $('.overlay').addClass('open');
-  });
-
-  $('.close-menu').on('click', function() {
-    $('.overlay').removeClass('open');
-  });
-});
