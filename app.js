7a70a64a05425edd24e7868f356f0039c98a8568
CoreNebula <62679387+CoreNebula@users.noreply.github.com>
2020-03-30 10:10:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,21 +6,15 @@ $(document).ready(function() {
   $('.close-menu').on('click', function() {
     $('.overlay').removeClass('open');
   });
+  $(".close-menu"):hover(function () {
+          $(this).removeClass('out').addClass('over');
+        console.log('Ayy!');
+  },function () {
+          $(this).removeClass('over').addClass('out');
+  });
+  $("li"):hover(function () {
+          $(this).removeClass('out').addClass('over');
+  },function () {
+          $(this).removeClass('over').addClass('out');
+  });
 });
-$(".close-menu"):hover(
-    function () {
-        $(this).removeClass('out').addClass('over');
-      console.log('Ayy!');
-    },
-    function () {
-        $(this).removeClass('over').addClass('out');
-    }
-);
-$("li"):hover(
-    function () {
-        $(this).removeClass('out').addClass('over');
-    },
-    function () {
-        $(this).removeClass('over').addClass('out');
-    }
-);
