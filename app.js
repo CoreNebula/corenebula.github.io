ad57b97aa24b3b05e4f6e922a89a440c25365594
CoreNebula <62679387+CoreNebula@users.noreply.github.com>
2020-04-01 01:08:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ $(document).ready(function() {
 	
 	$('.open-menu').click(function() {
 		$('.overlay').addClass('open');
-		location.href = "./index.html";
 	});
 
 	$('.close-menu').click(function() {
 		$('.overlay').removeClass('open');
-		location.href = "./pages/home"
 	});
 	
 	$('li').click(function() {
