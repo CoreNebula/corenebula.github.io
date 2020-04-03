938e2b4042b0a3b4f8b61f7b65e40f2459e435f2
CoreNebula <62679387+CoreNebula@users.noreply.github.com>
2020-04-03 01:49:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	
 	includeHTML('corenebula.github.io/pages/home.html');
 	
-	$('.fade-in').marginTop($(this).marginTop + 30).addClass('loaded');
+	$('.fade-in').marginTop($(this).marginTop + 30).opacity(1);
 	
 	$('.open-menu').click(function() {
 		$('.overlay').addClass('open');
