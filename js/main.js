d001c71ac9844473cf40a8ddf35788a8b56e7013
Nebula <62679387+CoreNebula@users.noreply.github.com>
2020-04-07 20:27:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -51,6 +51,7 @@ function fadeIn() {
 	z = document.getElementsByClassName('fade-in');
 	for (i=0;i < (z.length - 1);i++) {
 		elmnt = z[i];
+		console.log(got here);
 		if ((elmnt.offsetTop < window.innerHeight)&&(elmnt.offsetTop > 60)) {
 			elmnt.classList.add('in');
 		}
