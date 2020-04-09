076f04cfff4bdf819db89f19298e5f08dd65b92e
Nebula <62679387+CoreNebula@users.noreply.github.com>
2020-04-08 21:02:19 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	
 	sortList();
 	
-	includeHTML('corenebula.github.io/pages/home.html');
+	//includeHTML('corenebula.github.io/pages/home.html');
 		
 	$('.open-menu').click(function() {
 		$('.overlay').addClass('open');
